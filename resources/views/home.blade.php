03891a1f146c0dcabf93bdb4fc3b8792bee33a2b
Manuel della Gala <manuel.dellagala@gmail.com>
2023-02-01 22:43:09 +0100

    mod

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,34 +4,31 @@
 <div class="container">
     <div class="row justify-content-center">
 
-
-
-
                 @foreach ($posts as $post)
                 <div class="col-md-6 mb-2">
-                <div class="card">
+                    <div class="card">
 
-                    <div class="card-header">{{ __($post->title) }}</div>
+                        <div class="card-header">{{ __($post->title) }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-
-                        <img src="{{ asset('img/').'/'.$post->file }}" alt="">
-                        <p>{{ $post->description }}</p>
-                        <code>{{ $post->categories->name }}</code>
-                        @foreach ($post->tag as $tag)
-                            <code>{{ ($tag->name) }}</code>
-                        @endforeach
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
 
 
+                            <img src="{{ asset('img/').'/'.$post->file }}" alt="">
+                            <p>{{ $post->description }}</p>
+                            <code>{{ $post->categories->name }}</code>
+                            @foreach ($post->tag as $tag)
+                                <code>{{ ($tag->name) }}</code>
+                            @endforeach
+
+
+                        </div>
                     </div>
                 </div>
-            </div>
                         @endforeach
 
 
