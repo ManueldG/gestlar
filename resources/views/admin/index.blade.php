b042de84c2ab2abeb2db43480978d33c3600511b
Manuel della Gala <manuel.dellagala@gmail.com>
2023-02-02 20:33:55 +0100

    btn color

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -14,7 +14,7 @@
                             </div>
                         @endif
 
-                        <a href="{{ route('post.create') }}"> Crea nuovo post </a>
+                        <a class="btn btn-info" href="{{ route('post.create') }}"> Crea nuovo post </a>
 
                         <table class="table">
 
